b329088f9cb7358a877cc674792ea1c531d33560
voyager003 <miraclex000@gmail.com>
2024-10-14 22:52:01 +0900

    feat: 프로필 수정

--- src/libs/i18n/ui.ts
+++ src/libs/i18n/ui.ts
@@ -10,57 +10,32 @@ export const defaultLang = 'ko';
 export const ui = {
   en: {
     '404.desc': 'Cannot find the related document.',
-    'nav.title': 'My Space',
+    'nav.title': 'Category',
     'nav.writing': 'Writing',
-    'nav.writing.desc': 'A space for storing insights',
-    'nav.note': 'Note',
-    'nav.note.desc': 'A space for recording useful knowledge',
+    'nav.writing.desc': 'A place to write tech-article',
+    'nav.note': 'Imagery',
+    'nav.note.desc': 'A place to write down my thoughts',
     'nav.craft': 'Craft',
-    'nav.craft.desc': 'A space for creating something interesting',
-    'index.name': 'Edward Kim',
+    'nav.craft.desc': 'A place to write trial and error',
+    'index.name': 'Wongi Rim',
     'index.desc':
-      `<p>Passionate about interacting <b>smoothly</b> with users and <b>simplifying</b> complex things into simple and convenient forms.</p>` +
-      `<p>Enjoy exploring and reflecting, and aim to record discoveries along the way.</p>`,
+      `<p>I'm recording to doesn't volatilize.</p>`,
     'index.currentWork': `<p>
-      Currently work in 
-      <b>
-        <a class="external-link" href="https://www.kakaocorp.com/page/" target="_blank"
-          >Kakao</a
-        >
-      </b>
-      developing
-      <b>
-        <a class="external-link" href="https://brunch.co.kr/" target="_blank"
-          >Brunch</a
-        >
-      </b>.
     </p>`,
   },
   ko: {
     '404.desc': '관련된 문서를 찾을 수 없습니다.',
-    'nav.title': '나의 공간',
-    'nav.writing': '서재',
-    'nav.writing.desc': '인사이트를 보관하는 공간',
-    'nav.note': '수첩',
-    'nav.note.desc': '유용한 지식을 기록하는 공간',
+    'nav.title': '카테고리',
+    'nav.writing': '개발',
+    'nav.writing.desc': '개발 관련 글을 적는 곳',
+    'nav.note': '심상',
+    'nav.note.desc': '생각을 적는 곳',
     'nav.craft': '공방',
-    'nav.craft.desc': '무언가 흥미로운 것을 만드는 공간',
-    'index.name': '김평안',
+    'nav.craft.desc': '시행 착오를 기록하는 곳',
+    'index.name': '임원기',
     'index.desc':
-      `<p>사용자와 <b>부드럽게</b> 상호작용하는 것, 복잡한 것을 <b>단순하고 간편하게</b> 만드는 것에 열광합니다.</p>` +
-      `<p>탐구하고 사색하는 것을 좋아하여 발견한 것을 틈틈이 기록하려 합니다.</p>`,
+      `<p>휘발되지 않도록 <b>기록</b>을 남기고 있습니다.</p>`,
     'index.currentWork': `      <p>
-      현재
-      <b>
-        <a class="external-link" href="https://www.kakaocorp.com/page/" target="_blank"
-          >카카오</a
-        >
-      </b>에서
-      <b>
-        <a class="external-link" href="https://brunch.co.kr/" target="_blank"
-          >브런치</a
-        >
-      </b>를 개발하고 있습니다.
     </p>`,
   },
 } satisfies Record<Language, { [key: string]: string }>;
