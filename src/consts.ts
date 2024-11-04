5a6ca5296950f8b1fb2da295efcd74e356320df1
voyager003 <miraclex000@gmail.com>
2024-11-04 14:07:26 +0900

    refactor: 블로그 설정 변경 open graph, copy link 수정

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 
 export const SITE = {
-  title: 'devro.me',
+  title: 'romedev.me',
   titleTemplate: '%s',
-  description: 'Develop about something Soft and Simple.',
+  description: 'Ignis aurum probat, miseria fortes uiros.',
   image:
-    'https://user-images.githubusercontent.com/65283190/297786605-2009ebe2-8d44-47fd-949a-bdcdc0f71b11.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDU1OTA5NzMsIm5iZiI6MTcwNTU5MDY3MywicGF0aCI6Ii82NTI4MzE5MC8yOTc3ODY2MDUtMjAwOWViZTItOGQ0NC00N2ZkLTk0OWEtYmRjZGMwZjcxYjExLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMTglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTE4VDE1MTExM1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTQzMDgwYjlkMmU4MDg3MWNkMjU0NDg4MGVkMjMwMDZhZTBlMDg5MmE5ZjhiYWM1ZmU5MGVhNjNiMWQwYWEzYmMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.mmQksy-q_rFUVyC8RaA6la8cUXk_3VxZbfUDSaTirh8',
+    'https://github.com/user-attachments/assets/9358a1f3-e073-4d31-a851-305dfca3a025',
 };
